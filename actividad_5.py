2a88802de457f1c37f7d66141bd0d37e7ae677e4
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-22 09:41:24 -0600

    1Conmit menu de opciones

---
+++ actividad_5.py
@@ -0,0 +1,11 @@
+#actividad 5 en clase
+while True:
+    print("____Menu de analisis de ventas____")
+    print("op.1 Ingresar lista de ventas: ")
+    print ("op2. Mostrar todas las ventas ingresadas: ")
+    print("op3. Calcular la venta más alta y la más baja: ")
+    print("op4. Calcular promedio de ventas: ")
+    print("op5. Contar cuantos días superaron los Q1000: ")
+    print("op.6 Buscar si una  venta especifica existe en la lista: ")
+    print("op.7 Clasificar cada venta: alta(>1000), medio (500-1000), baja (<500)")
+    print("op.8  Salir")
